daa40e25ed1e51a6324c49aa2c40244e5553dae2
sawinkumar13-design <sawinkumar13@gmail.com>
2026-01-26 18:44:11 +0530

    [sawin] i removed my file

--- basic_constrution/AssignmentOpretor.java
+++
@@ -1,9 +0,0 @@
-class ArithmeticOpretor{
- public staic voide main(String args[]){
- int a = 10;
- int b = 20;
- a+=10
- System.out.println(sum);
- b-=20;
- }
-}
--- basic_constrution/unaryOpretor.java
+++
@@ -1,8 +0,0 @@
-class UnaryOpretor{
- public staic voide main(String args[]){
- int a = 10;
- System.out.println(a++);
- System.out.println(++a);
- System.out.println(a--);
- System.out.println(--a);
-}
