6bfff36c73d50949197b31e95ef3bff45042591d
sawinkumar13-design <sawinkumar13@gmail.com>
2026-03-16 19:03:06 +0530

    [sawin] add my file

---
+++ Package/src/mainClass.java
@@ -0,0 +1,31 @@
+import College.Faculty.Faculty;
+import College.Student.Student;
+import com.university.department.cse.course;
+import library.book.Book;
+import library.Trancastion.Trancastion;
+import library.member.member;
+
+
+public class mainClass {
+    public static void main(String[] args) {
+
+        Student s = new Student();
+        Faculty f = new Faculty();
+
+        s.displayStudent();
+        f.displayFaculty();
+
+        course c = new course();
+        c.showCourse();
+
+
+        Book b = new Book();
+        member m = new member();
+        Trancastion t = new Trancastion();
+
+        b.addBook();
+        m.registerMember();
+        t.issueBook();
+
+    }
+    }
